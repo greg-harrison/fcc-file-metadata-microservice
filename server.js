e7746b72edfae2561ad836b917a6c8884cfc3fe8
Vic <vic@minustime.com>
2017-10-31 22:40:16 -0400

    Setup to run Prettier on pre-commits

--- server.js
+++ server.js
@@ -1,53 +1,53 @@
-const express = require('express')
-const pug = require('pug')
-const multer = require('multer')
-const middleware = require('./middleware')
+const express = require("express");
+const pug = require("pug");
+const multer = require("multer");
+const middleware = require("./middleware");
 
-const landingTemplate = pug.compileFile('templates/landing.pug')
-const resultsTemplate = pug.compileFile('templates/results.pug')
+const landingTemplate = pug.compileFile("templates/landing.pug");
+const resultsTemplate = pug.compileFile("templates/results.pug");
 
-const app = express()
+const app = express();
 
-const storage = multer.memoryStorage()
-const upload = multer({ storage })
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 function bytesToSize(bytes) {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
-  if (bytes === 0) return 'n/a'
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
-  if (i === 0) return `${bytes} ${sizes[i]}`
-  return `${(bytes / (1024 * i)).toFixed(1)} ${sizes[i]}`
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  if (bytes === 0) return "n/a";
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+  if (i === 0) return `${bytes} ${sizes[i]}`;
+  return `${(bytes / (1024 * i)).toFixed(1)} ${sizes[i]}`;
 }
 
-app.use(middleware.malformedUrl)
+app.use(middleware.malformedUrl);
 
-app.get('/uploader', (req, res) => {
-  res.status(200).send(landingTemplate())
-})
+app.get("/uploader", (req, res) => {
+  res.status(200).send(landingTemplate());
+});
 
-app.post('/results', upload.single('file'), (req, res) => {
+app.post("/results", upload.single("file"), (req, res) => {
   let formData = req.body;
-  let sizeConversion = bytesToSize(req.file.size)
-  let showConversion = sizeConversion.indexOf('Bytes') === -1
-  let payload = {}
+  let sizeConversion = bytesToSize(req.file.size);
+  let showConversion = sizeConversion.indexOf("Bytes") === -1;
+  let payload = {};
 
   if (!req.file) {
-    res.redirect('/uploader')
+    res.redirect("/uploader");
   }
 
-  payload.fileName = req.file.originalname || null
-  payload.fileSize = req.file.size || null
+  payload.fileName = req.file.originalname || null;
+  payload.fileSize = req.file.size || null;
   if (showConversion) {
-    payload.fileSizeConverted = sizeConversion
+    payload.fileSizeConverted = sizeConversion;
   }
-  res.status(200).send(resultsTemplate({ payload }))
+  res.status(200).send(resultsTemplate({ payload }));
 });
 
 app.use((req, res) => {
   //Catch 404's and send them back to /uploader
-  res.redirect('/uploader')
-})
+  res.redirect("/uploader");
+});
 
 app.listen(8000, () => {
-    console.log('listening on localhost:8000');
+  console.log("listening on localhost:8000");
 });
